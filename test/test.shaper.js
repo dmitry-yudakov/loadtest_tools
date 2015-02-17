2594285156053892ea97d073f2644862f4315478
Dmitry Yudakov <dmitry.yudakov@gmail.com>
2015-02-17 17:06:58 +0200

    added automatic tests

---
+++ test/test.shaper.js
@@ -0,0 +1,80 @@
+'use strict';
+
+var assert = require('chai').assert,
+	expect = require('chai').expect,
+	chai = require('chai'),
+	should = chai.should(),
+	shaperCreator = require('../shaper.js');
+
+var shaper;
+
+describe("Shaper", function () {
+	describe("Exports", function () {
+
+		it('Shaper Creator should export create function', function () {
+			assert.isFunction(shaperCreator.create,
+				'does not export create()');
+		});
+
+		shaper = shaperCreator.create({
+			active_dialogs: 10,
+			callback_new: function () {}
+		});
+		assert.isObject(shaper, 'created shaper is not an object');
+
+		it('Shaper should export start function', function () {
+			assert.isFunction(shaper.start,
+				'shaper doesn\'t export start()');
+		});
+
+	});
+
+	describe("Callbacks", function () {
+		it('should test single instance interfaces', function (done) {
+			shaper = shaperCreator.create({
+				active_dialogs: 1,
+				callback_new: function (cbResp, cbDone) {
+					assert.isFunction(cbResp, 'callback Response is not a function');
+					assert.isFunction(cbDone, 'callback Done is not a function');
+					// leave it hanging for the moment
+					//				cbDone();
+					done();
+				}
+			});
+			shaper.start();
+		});
+
+	});
+
+	describe("Accuracy", function () {
+		it('should test the speed', function (done) {
+			var count = 0;
+			shaper = shaperCreator.create({
+				active_dialogs: 4,
+				callback_new: function (cbResp, cbDone) {
+					++count;
+					setTimeout(function () {
+						cbDone();
+					}, 20);
+				}
+			});
+			shaper.start();
+			setTimeout(function () {
+				expect(count).to.equal(4);
+			}, 10);
+			setTimeout(function () {
+				expect(count).to.equal(8);
+			}, 30);
+			setTimeout(function () {
+				expect(count).to.equal(12);
+			}, 50);
+			setTimeout(function () {
+				expect(count).to.equal(16);
+				done();
+			}, 70);
+		});
+
+	});
+
+
+});
