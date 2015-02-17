30489e15a5067bda0b6ab6b8a68d33d0ce233110
Dmitry Yudakov <dmitry.yudakov@gmail.com>
2015-02-17 16:53:36 +0200

    added sample script

---
+++ samples/test.js
@@ -0,0 +1,22 @@
+var shaper = require('./shaper.js').create({
+	active_dialogs: 2,
+	callback_new: newDialog
+});
+
+var countThisSecond = 0;
+setInterval(function() {
+	console.log(countThisSecond + ' dialogs started this second');
+	countThisSecond = 0;
+}, 1000);
+
+
+function newDialog(cbOnResp, cbDone) {
+//	console.log('Create new dialog');
+	++countThisSecond;
+	setTimeout(function(){
+//		console.log('Close dialog');
+		cbDone();
+	}, 1000);
+}
+
+shaper.start();
